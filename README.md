7e4bf32fb3710843ef23dd3b79b43803cc09ac51
franciscocruz29 <franciscohuertas25@gmail.com>
2022-05-10 06:53:59 -0500

    Added test and docs

---
+++ README.md
@@ -0,0 +1,83 @@
+# Favs-Api
+## Description
+Favs is a new company that aims to provide a better way to organize your favorite things: music, clothes, courses, etc., all in one place.
+
+## Installation
+Create a .env file with the following properties:
+```
+MONGODB_URI=[Add DB connection here]
+MONGODB_TEST_URI=[Add DB test connection here]
+TOKEN_KEY=[Add token key]
+```
+
+Installation is done using the
+[`npm install` command](https://docs.npmjs.com/getting-started/installing-npm-packages-locally):
+
+```console
+$ npm install
+```
+
+and
+
+```console
+$ npm start
+```
+
+## Testing
+In order to run test cases:
+
+```console
+$ npm run test
+```
+
+## Endpoints
+
+| Route	| HTTP Verb | Description |
+| :--- | :--- | :--- |
+| /api/favs | GET	|	Get all list of favorites |
+| /api/favs | POST | Creates a new list of favorites |
+| /api/favs/:id | GET | Get a single list of favorites |
+| /api/favs/:id | DELETE |	Deletes a list of favorites |
+| /auth/local/login | POST |	Login user by email/password |
+
+## Documentation
+[API DOCS](https://documenter.getpostman.com/view/20215269/Uyxepohf)
+
+## Authorization
+
+To authenticate an API request, you should provide your API key in the `Authorization` header.
+
+## Request bodies
+
+### Create user
+**Request:**
+```json
+{
+    "email": "testEmail@mail.com",
+    "password": "1234567" 
+}
+```
+
+### Login
+**Request:**
+```json
+{
+    "email": "testEmail@gmail.com",
+    "password": "1234567" 
+}
+```
+### Create a new list of favorites
+
+**Request:**
+```json
+{
+    "name": "Fav List 1",
+    "favs": [
+        {
+            "title": "Fav 1",
+            "description": "Description fav 1",
+            "link": "https://url.com"
+        }
+    ]
+}
+```
---
+++ src/index.test.js
@@ -0,0 +1,86 @@
+const req = require("supertest");
+const jwt = require("jsonwebtoken");
+const User = require("./api/user/user.model");
+const FavList = require("./api/fav-list/models/fav-list.model");
+const mongoose = require("mongoose");
+const { disconnectDB, connectDB, cleanupDB } = require("./config/database");
+const { app } = require("./index");
+
+const userId = mongoose.Types.ObjectId();
+const favListId = mongoose.Types.ObjectId();
+const user = {
+  userId,
+  email: "test@gmail.com",
+  password: "12345678",
+  token: jwt.sign({ userId, email: "test@gmail.com" }, process.env.TOKEN_KEY),
+};
+
+describe("User", () => {
+  beforeAll(async () => {
+    await connectDB();
+    await cleanupDB();
+
+    const defaultUser = new User(user);
+    await defaultUser.save();
+    const favList = new FavList({
+      _id: favListId,
+      name: "Test Favorites",
+      favs: [
+        {
+          title: "Test Favorite",
+          description: "this is a description favorite",
+          link: "https://google.com",
+        },
+      ],
+    });
+    await favList.save();
+  });
+  afterAll(async () => {
+    await cleanupDB();
+    await disconnectDB();
+  });
+  it("should log in user", async () => {
+    const res = await req(app)
+      .post("/auth/local/login")
+      .send({ email: "test@gmail.com", password: "12345678" });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.token).not.toBe(null);
+  });
+  it("should get fav lists", async () => {
+    const res = await req(app)
+      .get("/api/favs")
+      .set("Authorization", `Bearer ${user.token}`);
+    expect(res.statusCode).toBe(200);
+  });
+  it("should get fav list by id", async () => {
+    const res = await req(app)
+      .get(`/api/favs/${favListId.toString()}`)
+      .set("Authorization", `Bearer ${user.token}`);
+    expect(res.statusCode).toBe(200);
+    expect(res.body).not.toBe(null);
+  });
+  it("should create fav list", async () => {
+    const favList = {
+      name: "Fav List test",
+      favs: [
+        {
+          title: "Fav test",
+          description: "Description fav test",
+          link: "https://google.com",
+        },
+      ],
+    };
+    const res = await req(app)
+      .post("/api/favs")
+      .set("Authorization", `Bearer ${user.token}`)
+      .send(favList);
+    expect(res.statusCode).toBe(201);
+    expect(res.body).not.toBe(null);
+  });
+  it("should delete fav list by id", async () => {
+    const res = await req(app)
+      .delete(`/api/favs/${favListId.toString()}`)
+      .set("Authorization", `Bearer ${user.token}`);
+    expect(res.statusCode).toBe(200);
+  });
+});
